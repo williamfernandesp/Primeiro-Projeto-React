af3321f1189edcedf40ac4b83bf278b850ace71b
William Fernandes Pereira <williamfernandesp@outlook.com>
2022-02-15 20:46:49 -0300

    Aulas Compenents Header

--- willmoney/src/App.tsx
+++ willmoney/src/App.tsx
@@ -1,14 +1,11 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
