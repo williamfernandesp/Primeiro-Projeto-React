fd0fb23f08bd08fcf1ba0bccce710fa6e8fe587a
William Fernandes Pereira <williamfernandesp@outlook.com>
2022-03-14 14:44:18 -0300

    Continuando

--- willmoney/src/components/Summary/index.tsx
+++ willmoney/src/components/Summary/index.tsx
@@ -1,10 +1,15 @@
+import React, { useContext } from "react";
 import incomeImg from "../../assets/entradas.svg";
 import outcomeImg from "../../assets/saídas.svg";
 import totalImg from "../../assets/total.svg";
-
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext);
+
+  console.log(transactions);
+
   return (
     <Container>
       <div>
